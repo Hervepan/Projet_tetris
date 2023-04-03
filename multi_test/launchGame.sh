9dee232d3e518b5185dfabe0245cd122fd0111f7
HPan <hervepan@outlook.fr>
2023-04-03 15:38:46 +0200

    Last

--- multi_test/launchGame.sh
+++ multi_test/launchGame.sh
@@ -12,6 +12,12 @@ fi
 clear
 NEWPS1='${PS1/32/'$COLOR'}'
 echo "Please type the following command in the terminal to help see in which network space you are in"
-echo "PS1=$NEWPS1"
+echo "PS1=$NEWPS1" 
+echo "To launch the game go into projectC++ and type the command :"
+if [[ $1 == "red" ]]; then
+  echo '    make run ARGS="server"' 
+elif [[ $1 == "blue" ]]; then
+  echo 'make run ARGS="client 192.168.15.1"'
+fi
 sudo ip netns exec $1 bash -c "su $USER"
 
